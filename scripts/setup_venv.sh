378280c2eebee9e2efc4da18bfe1c5466e795e29
cwintermute <111081079+cwintermute@users.noreply.github.com>
2025-01-22 13:28:16 -0800

    Pinned older versin of bottle to get around deprecation warnings causing 500 errors

--- scripts/setup_venv.sh
+++ scripts/setup_venv.sh
@@ -2,5 +2,5 @@ mkdir -p ~/indi-web
 cd ~/indi-web
 python3 -m venv venv
 source venv/bin/activate
-pip3 install indiweb importlib-metadata
+pip3 install bottle==0.12.25 importlib-metadata indiweb
 deactivate
