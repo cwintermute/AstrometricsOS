032a2dde6d338dec2e5ea38fd5752d607a9be1bd
cwintermute <111081079+cwintermute@users.noreply.github.com>
2025-01-22 16:40:55 -0800

    Have script rename itself after run to prevent multiple runs

--- scripts/fix_desktop_icons.sh
+++ scripts/fix_desktop_icons.sh
@@ -1,1 +1,3 @@
 for f in /home/seven/Desktop/*.desktop; do chmod +x "$f"; dbus-launch gio set -t string "$f" metadata::xfce-exe-checksum "$(sha256sum "$f" | awk '{print $1}')"; done
+# Rename this file to prevent multiple runs lol
+mv $0 "ran_$0"
